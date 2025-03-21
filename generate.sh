66a72d402bfa8be1163cba676180bdf40657a76e
Russell Wong <russellwmy@gmail.com>
2025-03-21 13:23:24 +0000

    updated package versiona dn re-generate code

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 for NAME in Tensor SparseTensor Schema Message File
 do
-	curl https://raw.githubusercontent.com/apache/arrow/master/format/$NAME.fbs -o $NAME.fbs
+	curl https://raw.githubusercontent.com/apache/arrow/main/format/$NAME.fbs -o $NAME.fbs
 done
 
-planus rust *.fbs -o src/ipc/generated.rs
+planus --ignore-docstring-errors rust *.fbs -o src/ipc/generated.rs 
